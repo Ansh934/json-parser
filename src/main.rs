3899c5318a93993a704f20c58cdb1f0a3f4e3e60
Ansh934 <soni.ansh934@gmail.com>
2026-05-12 03:18:08 +0530

    Refactor main function: Update file reading to use command line argument and improve output formatting

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,22 @@ mod parser;
 mod token;
 mod error;
 
+use std::env;
 use crate::lexer::*;
 use crate::parser::*;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let content = std::fs::read_to_string("test.json")?;
-    println!("\nContent of test.json:");
-    println!("{}", content);
+    let file_path = env::args().nth(1).unwrap_or_else(|| "default_test.json".into());
+    println!("\nReading file: {}", file_path);
+    let content = std::fs::read_to_string(&file_path)?;
+    println!("Content of {}: \n{}", file_path, content);
 
     let lexer = Lexer::tokenize(content.chars())?;
     println!("\nTokens:");
     for token in &lexer.tokens {
-        println!("{:?}", token);
+        print!("{:?} | ", token);
     }
+    println!();
 
     let parser = Parser::parse(lexer.tokens)?;
     println!("\nParsed JSON:");
